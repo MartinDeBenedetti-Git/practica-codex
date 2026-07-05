60414aeb54d1613084ff48aeb4ea971de3040516
Martín De Benedetti <debenedettimartin16@gmail.com>
2026-07-05 01:10:12 -0300

    agrego cambios

--- productos.js
+++ productos.js
@@ -53,6 +53,7 @@ const productos=[
 function listarProductos(){
     productos.forEach(producto => {
         console.log(`ID: ${producto.id} - Nombre: ${producto.nombre} - Precio: ${producto.precio} - Categoria: ${producto.categoria} - Stock: ${producto.stock}`);
+        return productos;
     });
 }
 
@@ -73,30 +74,33 @@ function filtarProductosPorCategoria(categoria){
     console.log(`Total de productos filtrados: ${productosFiltrados.length}`);
 }
 
+function filtarProductosPorCategoria(categoria){
+    return productos.filter(producto => producto.categoria === categoria);  //devuelve un array con los productos que cumplen la condición
+}                                                                           //el filter() devuelve un array con los elementos que cumplen la condición, en este caso, los productos que pertenecen a la categoría especificada
+
+
 function crearProducto(nombre, precio, categoria, stock){
+    const nuevoID = productos.length + 1;
     const producto = {
-        id:productos.length + 1,
+        id: nuevoID,
         nombre,
         precio,
         categoria,
         stock
     }
-    productos.push(producto);
+    productos.push(producto);  //push() agrega un elemento al final del array
     console.log("El producto se ha creado correctamente");
     console.log(`ID: ${producto.id} - Nombre: ${producto.nombre} - Precio: ${producto.precio} - Categoria: ${producto.categoria} - Stock: ${producto.stock}`);
 }
 
-function actualizarProducto(id, nombre, precio, categoria, stock){
+function actualizarProducto(id, datosActualizados){
     const producto = productos.find(producto => producto.id === id);
     if(producto === undefined){
         console.log("Producto no encontrado");
         return;
     }
     else{
-        producto.nombre = nombre;
-        producto.precio = precio;
-        producto.categoria = categoria;
-        producto.stock = stock;
+        Object.assign(producto, datosActualizados);  //Object.assign() permite actualizar las propiedades de un objeto con las propiedades de otro objeto
         console.log("El producto se ha actualizado correctamente");
         console.log(`ID: ${producto.id} - Nombre: ${producto.nombre} - Precio: ${producto.precio} - Categoria: ${producto.categoria} - Stock: ${producto.stock}`);
     }
@@ -110,7 +114,7 @@ function eliminarProducto(id){
     }
     else{
         const productoIndex = productos.indexOf(producto);
-        productos.splice(productoIndex, 1);
+        productos.splice(productoIndex, 1);   //el splice elimina el producto del array
         console.log("El producto se ha eliminado correctamente");
     }
 }
@@ -122,6 +126,13 @@ function calcularValorTotalInventario(){
     });
     console.log(`El valor total del inventario es de ${valorTotalInventario}`);
 }
+
+function calcularValorTotalInventario() {
+    return productos.reduce((total, producto) => {
+        return total + producto.precio * producto.stock;
+    }, 0);  //el cero es el valor inicial, el reduce() llama a la función de acuerdo a cada elemento del array, y devuelve el resultado final
+}
+
 
 
 
